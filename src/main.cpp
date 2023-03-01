9fde2dce137abb7f61e1b9e90a50a5cb62c8b486
folosuru <folosuru@gmail.com>
2023-03-01 16:16:03 +0900

    もう全部threadでやっちゃう

--- src/main.cpp
+++ src/main.cpp
@@ -18,49 +18,43 @@ int main() {
         }
     }
 
-    auto count = 5;
-    std::unordered_set<std::string> keys;
-    redis.scan( 0LL, "misskey_tool:queue*", count, std::inserter(keys, keys.begin()));
 
     std::vector<std::thread> thread_list;
-    std::vector<std::future<std::optional<api::instance_list>>> result_list;
-    for (auto item : keys) {
-        redis.del(item);
-        item.erase(0, 19);
-        redis.set("misskey_tool:history:" + item,"");
-        std::promise<std::optional<api::instance_list>> promise;
-        result_list.push_back(promise.get_future());
-        thread_list.emplace_back([](std::string url , std::promise<std::optional<api::instance_list>> promise) {
+
+        thread_list.emplace_back(std::thread([]{
+            auto redis = Redis("tcp://127.0.0.1:6379");
+            std::unordered_set<std::string> keys;
+            redis.scan( 0LL, "misskey_tool:queue*", 1 , std::inserter(keys, keys.begin()));
+            std::string url = *keys.begin();
+            url.erase(0, 19);
+            redis.rename("misskey_tool:queue:" + url, "misskey_tool:history:" + url);
             std::cout << "get: " + url << std::endl;
-                if (url == "misskey.io") {
-                    promise.set_value(std::nullopt);
-                    return ;
+            if (url == "misskey.io") {
+                return;
+            }
+            auto i = api::getInstance(std::wstring(url.begin(), url.end()));
+            if (i == nullptr) {
+                std::cout << "nullptr" << std::endl;
+                return;
+            }
+            auto list = i->fetchAllFederation();
+
+            if (!list) return;
+            for (const auto& i1: list.value()) {
+                std::cout << i1 << std::endl;
+                if (redis.get("misskey_tool:*" + i1)){
+                    continue;
+                } else {
+                    redis.set("misskey_tool:queue:" + i1, "");
                 }
-                auto i = api::getInstance(std::wstring(url.begin(), url.end()));
-                if (i == nullptr) {
-                    std::cout << "nullptr" << std::endl;
-                    promise.set_value(std::nullopt);
-                    return;
-                }
-                promise.set_value(i->fetchAllFederation());
-            } , item , std::move(promise)
-        );
-    }
+            }
+        }));
+
 
     for (auto& thread : thread_list) {
         thread.join();
     }
-    for (auto& future : result_list ){
-        auto result = future.get();
-        if (!result) continue;
-        for (const auto& i1: result.value()) {
-            if (redis.get("misskey_tool:*" + i1)){
-                continue;
-            } else {
-                redis.set("misskey_tool:queue:" + i, "");
-            }
-        }
-    }
+
     return 0;
 }
 
