6ac1eae454ea60cd54ba65c49af2b3811546d6f3
folosuru <folosuru@gmail.com>
2023-03-06 22:27:55 +0900

    ますとどん～～

---
+++ src/software/mastodon.cpp
@@ -0,0 +1,22 @@
+//
+// Created by folosuru on 2023/03/06.
+//
+
+#include "mastodon.hpp"
+
+std::string mastodon::getDescription() {
+    std::string description_v1 = web::http::client::http_client(getURL() + API_V1_INSTANCE)
+        .request(web::http::methods::GET).get().extract_utf8string().get();
+    if (description_v1[0] == '[' || description_v1[0] == '{') {
+        return nlohmann::json::parse(description_v1)["short_description"].get<std::string>();
+    }
+    std::string description_v2 = web::http::client::http_client(getURL() + API_V2_INSTANCE)
+            .request(web::http::methods::GET).get().extract_utf8string().get();
+    if (description_v1[0] == '[' || description_v1[0] == '{') {
+        return nlohmann::json::parse(description_v1)["description"].get<std::string>();
+    }
+    return "";
+}
+
+const utility::string_t mastodon::API_V1_INSTANCE = utility::conversions::to_string_t("/api/v1/instance");
+const utility::string_t mastodon::API_V2_INSTANCE = utility::conversions::to_string_t("/api/v2/instance");
---
+++ src/software/mastodon.hpp
@@ -0,0 +1,16 @@
+//
+// Created by folosuru on 2023/03/06.
+//
+#pragma once
+#include <future>
+#include "../api.hpp"
+
+#include "nlohmann/json.hpp"
+
+class mastodon : public api {
+public:
+    std::string getDescription() override;
+
+    static const utility::string_t API_V1_INSTANCE;
+    static const utility::string_t API_V2_INSTANCE;
+};
