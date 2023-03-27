a1a503fe2c944afd35fae8927e3205c05a21fda1
folosuru <folosuru@gmail.com>
2023-03-27 21:58:56 +0900

    updater

---
+++ src/updater.cpp
@@ -0,0 +1,19 @@
+//
+// Created by folosuru on 2023/03/27.
+//
+#include "util/sql.hpp"
+#include "software_api/api.hpp"
+#include <pqxx/pqxx>
+
+int main() {
+    pqxx::connection connection = util::sql::createConnection();
+    pqxx::connection update_connection = util::sql::createConnection();
+    pqxx::work tx(connection);
+    for (auto [domain] : tx.query<std::string>(
+            "SELECT domain FROM instance_list limit 2"))
+    {
+        util::sql::updateInstance(update_connection, api::getInstance(domain));
+        std::cout << "update :" << domain << std::endl;
+    }
+    return 0;
+}
